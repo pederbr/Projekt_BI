789033ad6c08d3fc6f2415c23fa42e4dde0d6eda
pederbr <pedebr@stud.ntnu.no>
2024-01-02 17:17:25 +0100

    Add graphing functionality to process_csv_for_graph()

--- bedkom_inhouse/csv_format/utils.py
+++ bedkom_inhouse/csv_format/utils.py
@@ -1,6 +1,8 @@
 from .models import semestere, bedrift_data
 import pandas as pd
 import datetime as dt
+import matplotlib.pyplot as plt
+
 
 def check_semester():
     for semester in semestere.objects.all():
@@ -72,6 +74,70 @@ def process_csv_for_table(df, lunspres:bool)->tuple:
     return average_score_questions
 
 
+def process_csv_for_graph(df, lunspres:bool)->list:
+    #svarene på enten/eller spm
+    q_list_1=[   
+    df.loc[1:2],    #kvinner/menn
+    df.loc[6:7],    # data/komtek
+    df.loc[40:41]]  #kunne tenke seg å jobbe der
+
+
+    out_list_1=[]
+    for elem in q_list_1:
+        question=elem.values.tolist()
+        temp=[]
+        for elem in question:
+            if type(elem[1]) == str:
+                temp.append(tuple([elem[0] ,int(elem[1])] ) )
+        if temp:
+            out_list_1.append(temp)
+
+        #svarene på veldig_bra til veldig_dårlig spørsmålene
+    if lunspres:
+        q_list=[
+        df.loc[10:14],  #fordeling_klassetrinn
+        df.loc[65:69],  #fremforing_presentasjon
+        df.loc[72:76],  #kunnskap_bedrift_pre
+        df.loc[79:83],  #kunnskap_bedrift_post
+        df.loc[86:90],  #info_jobb
+        df.loc[58:62],  #innhold_presentasjon
+        df.loc[17:21],  #intterresant_arbeid
+        df.loc[33:37],  #sosialt_miljø
+        df.loc[44:48],  #arbeidsvilkår
+        df.loc[51:55],  #helhetsvurdering_bedrift
+        df.loc[104:108],#inntrykk_arrangement
+        df.loc[24:30]]  #hva bedriften tilbyr
+
+    else:
+        q_list=[
+        df.loc[10:14],  #fordeling_klassetrinn
+        df.loc[72:76],  #fremforing_presentasjon
+        df.loc[65:69],  #innhold_presentasjon
+        df.loc[79:83],  #kunnskap_bedrift_pre
+        df.loc[86:90],  #kunnskap_bedrift_post
+        df.loc[93:97],  #info_jobb
+        df.loc[58:62],  #mingling
+        df.loc[17:21],  #intterresant_arbeid
+        df.loc[33:37],  #sosialt_miljø
+        df.loc[44:48],  #arbeidsvilkår
+        df.loc[51:55],  #helhetsvurdering_bedrift
+        df.loc[111:115],#inntrykk_arrangement
+        df.loc[24:30]]  #hva bedriften tilbyr
+
+    
+    out_list_2=[]
+    for elem in q_list:
+        question=elem.values.tolist()
+        temp=[]
+        for elem in question:
+            if type(elem[1]) == str:
+                temp.append(tuple([elem[0] ,int(elem[1])] ) )
+        if temp:
+            out_list_2.append(temp)
+    print()
+    
+    return out_list_1 + out_list_2
+
 def get_semester(date_str:str)->str:
     date=dt.datetime.strptime(date_str, "%Y-%m-%d").date()
     year=str(date.year)
@@ -85,3 +151,23 @@ def get_semester(date_str:str)->str:
         if created:
             obj.save()
     return semester
+
+def draw_bar(data):
+   labels = [x[0] for x in data]
+   values = [x[1] for x in data]
+   fig, ax = plt.subplots()
+   ax.bar(labels, values)
+   ax.set_ylim([0, 30])
+   ax.set_ylabel('Antall')
+   plt.xticks(rotation=45, ha='right')  # Rotate labels
+   plt.show()
+
+def draw_pie(data):
+   labels = [x[0] for x in data]
+   sizes = [x[1] for x in data]
+   fig, ax = plt.subplots()
+   ax.pie(sizes, labels=labels, autopct=lambda p: '{:.0f}'.format(p * sum(sizes) / 100), startangle=90, pctdistance=0.85)
+   centre_circle = plt.Circle((0,0),0.25,fc='white') # Draw a circle at the center
+   fig.gca().add_artist(centre_circle)
+   ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+   plt.show()
